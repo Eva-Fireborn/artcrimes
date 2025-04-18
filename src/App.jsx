60b7d10d737437e97a62c70ff48aa87153cf56c7
Eva Fireborn <eva.fireborn@gmail.com>
2025-04-18 10:56:50 +0200

    Fetch initial data + handle error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from 'react';
+import { API_URL } from './data/settings';
+
 function App() {
+  const pageSize = 25;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isApiError, setIsApiError] = useState(false);
+  const [result, setResult] = useState(null);
+  const [resultTotal, setResultTotal] = useState(0);
+
+  const fetchArtcrimeData = async () => {
+    let headers = {};
+    if (import.meta.env.VITE_API_KEY) {
+      headers['X-Api-Key'] = import.meta.env.VITE_API_KEY
+    }
+
+    try {
+      const response = await fetch(`${API_URL}?pageSize=${pageSize}&page=${pageNumber}&sort_order=desc&sort_on=_score`, {
+        headers
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const jsonResponse = await response.json();
+      setPageNumber(jsonResponse?.page);
+      setResultTotal(jsonResponse?.total);
+      setResult(jsonResponse?.items);
+      console.log(jsonResponse);
+    } catch (error) {
+      console.error(error.message);
+      setIsApiError(true);
+    }
+  }
+
+  useEffect(() => {
+    fetchArtcrimeData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
-      <div>
-      </div>
+    <header><h1>National Stolen Art File</h1></header>
+    <main>
+      {isApiError && <p>An error occured, refresh your browser and try again.</p>}
+      {!isApiError && result && (
+        <div>
+          <h2 className='visually-hidden'>List of national stolen art</h2>
+          {resultTotal}
+        </div>
+      )}
+    </main>
     </>
   )
 }
